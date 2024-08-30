a7bc3a092d087143e30b77f527e9e43dc59165c9
umesh raut <143353687+umeshraut2002@users.noreply.github.com>
2024-08-30 04:57:36 +0000

    area circumference diameter higher order function

---
+++ fun_cal_area_cirrcumference_dimension.js
@@ -0,0 +1,30 @@
+// You are given an array of radii for multiple circles: [3, 1, 2, 4].Write JavaScript functions to calculate the following properties for each circle using higher - order functions
+
+let radi = [3,1,2,4];
+
+const area = function(radi){ 
+    return Math.PI * radi * radi;
+}
+
+const circumference = function (radi){
+    return 2 * Math.PI * radi;
+}
+
+const diameter = function (radi){
+    return 2 * radi;
+} 
+
+
+function calculate(radi, logic){
+    const output = [];
+    for(let i = 0; i < radi.length; i++){
+        output.push(Math.floor(logic(radi[i])));
+    }
+    return output;
+};
+
+console.log(`Area is ${calculate(radi, area)}`);
+console.log(`Circumference is ${calculate(radi, circumference)}`);
+console.log(`Diameter is ${calculate(radi, diameter)}`);
+
+
