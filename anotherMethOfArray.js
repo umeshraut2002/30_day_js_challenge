e77080758c702a0b28305763100ff8bbd2e0af0d
umesh raut <143353687+umeshraut2002@users.noreply.github.com>
2024-08-31 14:27:29 +0000

    isArray of from

---
+++ anotherMethOfArray.js
@@ -0,0 +1,12 @@
+console.log(Array.isArray("Umesh"));
+
+console.log(Array.from("Umesh"));
+
+let val1 = 10;
+let val2 = 20;
+let val3 = 30;
+let val4 = 40;
+let val5 = 50;
+let val6 = 60;
+
+console.log(Array.of(val1, val2, val3, val4, val5, val6));
