52b8360e4e3c870d1f8ca4f63db05392591623f9
umesh raut <143353687+umeshraut2002@users.noreply.github.com>
2024-09-01 12:40:05 +0000

    merge two object spread

---
+++ mergeTwoObj.js
@@ -0,0 +1,23 @@
+// merging two objects or more object in single way as output 
+
+const googleDev = {
+    employeeName: "Umesh",
+    id: "77364",
+    groupName: "AI Powred Cloud Platformm",
+    salary: 30000
+}
+
+const newGoogleFeature = {
+    groupSize: 7,
+    seniorDevNo: 2,
+    internNo: 2,
+    teamLead: "Abhishek",
+    Manager: "Urvi Goel"
+}
+
+// merging an above objects in one way 
+
+const obj = {...googleDev, ...newGoogleFeature};
+
+console.log(obj);
+
