6987033c8532422ac4c2ea63d6a4438253ccfe9b
umesh raut <143353687+umeshraut2002@users.noreply.github.com>
2024-09-10 13:42:30 +0000

    instanceof add

--- oops.js
+++ oops.js
@@ -7,8 +7,8 @@ function user(studentName, id, rollNumber, marks){
     return this;
 }
 
-const user1 = new user("umesh", 123, 45, 85);
-const user2 = new user("vihaan", 354, 77, 70);
-console.table(user1);
+const user1 = new user("umesh", 123, 45, 85,555);
+const user2 = new user("vihaan", 354, 77, 70, 777);
+console.table(user1 instanceof user);
 console.table(user2);
 
