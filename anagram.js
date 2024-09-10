2f40947eba4e3cd0fe54361e124b866a169901fe
umesh raut <143353687+umeshraut2002@users.noreply.github.com>
2024-09-10 09:27:54 +0000

    anagram basic

---
+++ anagram.js
@@ -0,0 +1,13 @@
+function anagram(str1, str2){
+    if (str1.length  === str2.length) {
+        return true;
+    }
+    else{
+        false;
+    }
+}
+
+const arr1 = "tan";
+const arr2 = "nat";
+
+console.log(anagram(arr1, arr2));
