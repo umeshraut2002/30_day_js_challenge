e51e9073bfa1209086fb6892ce95e40da6ee74d3
umesh raut <143353687+umeshraut2002@users.noreply.github.com>
2024-09-11 12:30:18 +0000

    localStorage add

--- callandbind.js
+++ callandbind.js
@@ -3,11 +3,15 @@ function setUserName(username){
 }
 
 function createUserName(username, email, number){
-    setUserName(username);
+    setUserName.call(this, username);
     this.email = email;
     this.number = number;
 }
 
 const chai = new createUserName("umesh", "@gmail.com", 3888383449);
 
-console.log(chai);
+console.table(chai);
+
+const tea = new setUserName("vihaan")
+
+console.log(tea);
---
+++ localStorages.js
@@ -0,0 +1,10 @@
+const userData = {
+    name: "Vihaan",
+    email: "uraut6044@gmail.com",
+    password: "123456789",
+    recoveryEmail: "uraut56666@gmail.com"
+}
+
+let myData = localStorage.setItem(userData);
+
+console.log(myData);
